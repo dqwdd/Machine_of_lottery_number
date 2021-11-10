95f7cc25afc35236ffb1b33d42b4c401baa309c7
dqwdd <dqwdd@naver.com>
2021-11-10 21:18:12 +0900

    Initial commit

---
+++ app/src/main/java/com/fastcampus/machine_of_lottery_number/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.fastcampus.machine_of_lottery_number
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
